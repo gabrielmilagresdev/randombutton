8a0e6e329a789d646a89151f71678320678e1b23
Gabriel Milagres <ghamilagres@gmail.com>
2025-05-04 15:00:27 -0300

    add function random_color

--- randombutton.py
+++ randombutton.py
@@ -4,7 +4,6 @@ import random
 root = tk.Tk()
 root.title = ("Random Button")
 root.geometry("300x300")
-
 canvas = tk.Canvas(root, width=300, height=300, bg="#FFFFFF", highlightthickness=2)
 canvas.pack()
 
@@ -13,60 +12,30 @@ yp = 250
 tp = 150
 xs = 60
 ys = 260
-
 xpa = xp+10
 ypa = yp+10
 tpa = tp+10
 
 
-nf='#'
-n1 = ['0','1','2','3','4','5','6','7','8','9','F','E','D','C','B','A']
-n2 = ['0','1','2','3','4','5','6','7','8','9','F','E','D','C','B','A']
-n3 = ['0','1','2','3','4','5','6','7','8','9','F','E','D','C','B','A']
-n4 = ['0','1','2','3','4','5','6','7','8','9','F','E','D','C','B','A']
-n5 = ['0','1','2','3','4','5','6','7','8','9','F','E','D','C','B','A']
-n6 = ['0','1','2','3','4','5','6','7','8','9','F','E','D','C','B','A']
-ln1 = random.choice(n1)
-ln2 = random.choice(n2)
-ln3 = random.choice(n3)
-ln4 = random.choice(n4)
-ln5 = random.choice(n5)
-ln6 = random.choice(n6)
-color = nf+ln1+ln2+ln3+ln4+ln5+ln6
-clr = color
-ln1 = random.choice(n1)
-ln2 = random.choice(n2)
-ln3 = random.choice(n3)
-ln4 = random.choice(n4)
-ln5 = random.choice(n5)
-ln6 = random.choice(n6)
-color = nf+ln1+ln2+ln3+ln4+ln5+ln6
-nclr = color
-
-circle = canvas.create_oval(xp, xp, yp, yp, fill=clr, outline="#000000", width=4)
-circle_shadow = canvas.create_oval(xs,xs,ys,ys,fill="#000000")
-canvas.tag_lower(circle_shadow, None) 
-text = canvas.create_text(tp,tp, text="Random Button", fill=nclr,font=("Arial", 14, "bold"))
-
-
+def random_color():
+    n1 = ['0','1','2','3','4','5','6','7','8','9','F','E','D','C','B','A']
+    n2 = ['0','1','2','3','4','5','6','7','8','9','F','E','D','C','B','A']
+    n3 = ['0','1','2','3','4','5','6','7','8','9','F','E','D','C','B','A']
+    n4 = ['0','1','2','3','4','5','6','7','8','9','F','E','D','C','B','A']
+    n5 = ['0','1','2','3','4','5','6','7','8','9','F','E','D','C','B','A']
+    n6 = ['0','1','2','3','4','5','6','7','8','9','F','E','D','C','B','A']
+    ln1 = random.choice(n1)
+    ln2 = random.choice(n2)
+    ln3 = random.choice(n3)
+    ln4 = random.choice(n4)
+    ln5 = random.choice(n5)
+    ln6 = random.choice(n6)
+    color = '#'+ln1+ln2+ln3+ln4+ln5+ln6
+    return color
 def on_click(event):
     x, y = event.x, event.y
-    ln1 = random.choice(n1)
-    ln2 = random.choice(n2)
-    ln3 = random.choice(n3)
-    ln4 = random.choice(n4)
-    ln5 = random.choice(n5)
-    ln6 = random.choice(n6)
-    color = nf+ln1+ln2+ln3+ln4+ln5+ln6
-    clr = color
-    ln1 = random.choice(n1)
-    ln2 = random.choice(n2)
-    ln3 = random.choice(n3)
-    ln4 = random.choice(n4)
-    ln5 = random.choice(n5)
-    ln6 = random.choice(n6)
-    color = nf+ln1+ln2+ln3+ln4+ln5+ln6
-    nclr = color
+    clr = random_color()
+    nclr = random_color()
     if not clr == nclr and xp <= x <= yp and xp <= y <= yp: 
         canvas.itemconfig(circle, fill=clr,)
         canvas.itemconfig(text, fill=nclr)
@@ -77,7 +46,14 @@ def on_click(event):
 def on_release(event):
     canvas.coords(circle,xp,xp,yp,yp)
     canvas.coords(text,tp,tp)
-    
+
+clr = random_color()
+nclr = random_color()
+
+circle = canvas.create_oval(xp, xp, yp, yp, fill=clr, outline="#000000", width=4)
+circle_shadow = canvas.create_oval(xs,xs,ys,ys,fill="#000000")
+canvas.tag_lower(circle_shadow, None) 
+text = canvas.create_text(tp,tp, text="Random Button", fill=nclr,font=("Arial", 14, "bold"))
 
 canvas.bind("<ButtonPress-1>", on_click)
 canvas.bind("<ButtonRelease-1>", on_release)
