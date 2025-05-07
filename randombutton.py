bbdb60e4906f966a797131a4d61a7a555ab2cd8f
Gabriel Milagres <ghamilagres@gmail.com>
2025-05-07 00:28:17 -0300

    add shape square (unstable)

--- randombutton.py
+++ randombutton.py
@@ -16,7 +16,6 @@ xpa = xp+10
 ypa = yp+10
 tpa = tp+10
 
-
 def random_color():
     n1 = ['0','1','2','3','4','5','6','7','8','9','F','E','D','C','B','A']
     n2 = ['0','1','2','3','4','5','6','7','8','9','F','E','D','C','B','A']
@@ -32,29 +31,72 @@ def random_color():
     ln6 = random.choice(n6)
     color = '#'+ln1+ln2+ln3+ln4+ln5+ln6
     return color
+
+def start_circle():
+    global z
+    global circle
+    global text
+    circle = canvas.create_oval(xp, xp, yp, yp, fill=clr, outline="#000000", width=4)
+    circle_shadow = canvas.create_oval(xs,xs,ys,ys,fill="#000000")
+    canvas.tag_lower(circle_shadow, None)
+    text = canvas.create_text(tp,tp, text="Random Button", fill=nclr,font=("Arial", 14, "bold")) 
+    z=1
+
+def shape_square(event):
+    global z
+    global square
+    global text
+    canvas.delete("all")
+    square = canvas.create_rectangle(xp, xp, yp, yp, fill=clr, outline="#000000", width=4)
+    square_shadow = canvas.create_rectangle(xs,xs,ys,ys,fill="#000000")
+    canvas.tag_lower(square_shadow, None)
+    text = canvas.create_text(tp,tp, text="Random Button", fill=nclr,font=("Arial", 14, "bold"))
+    z=0
+
+def shape_circle(event):
+    global z
+    global circle
+    global text
+    canvas.delete("all")
+    circle = canvas.create_oval(xp, xp, yp, yp, fill=clr, outline="#000000", width=4)
+    circle_shadow = canvas.create_oval(xs,xs,ys,ys,fill="#000000")
+    canvas.tag_lower(circle_shadow, None)
+    text = canvas.create_text(tp,tp, text="Random Button", fill=nclr,font=("Arial", 14, "bold")) 
+    z=1
+
 def on_click(event):
+    global z
     x, y = event.x, event.y
     clr = random_color()
     nclr = random_color()
-    if not clr == nclr and xp <= x <= yp and xp <= y <= yp: 
-        canvas.itemconfig(circle, fill=clr,)
-        canvas.itemconfig(text, fill=nclr)
-        canvas.coords(circle,xpa,xpa,ypa,ypa)
-        canvas.coords(text,tpa,tpa)
-        root.after(9999999999999, lambda: canvas.coords(circle,xp,xp,yp,yp))
-        root.after(9999999999999, lambda: canvas.coords(text,tp,tp))
+    if not clr == nclr and xp <= x <= yp and xp <= y <= yp and z==1: 
+            canvas.itemconfig(circle, fill=clr,)
+            canvas.itemconfig(text, fill=nclr)
+            canvas.coords(circle,xpa,xpa,ypa,ypa)
+            canvas.coords(text,tpa,tpa)
+            root.after(9999999999999, lambda: canvas.coords(circle,xp,xp,yp,yp))
+            root.after(9999999999999, lambda: canvas.coords(text,tp,tp))
+    if not clr == nclr and xp <= x <= yp and xp <= y <= yp and z==0: 
+            canvas.itemconfig(square, fill=clr,)
+            canvas.itemconfig(text, fill=nclr)
+            canvas.coords(square,xpa,xpa,ypa,ypa)
+            canvas.coords(text,tpa,tpa)
+            root.after(9999999999999, lambda: canvas.coords(square,xp,xp,yp,yp))
+            root.after(9999999999999, lambda: canvas.coords(text,tp,tp))
 def on_release(event):
-    canvas.coords(circle,xp,xp,yp,yp)
-    canvas.coords(text,tp,tp)
-
+        global z
+        if z==1:
+            canvas.coords(circle,xp,xp,yp,yp)
+        if z==0:
+            canvas.coords(square,xp,xp,yp,yp)
+        canvas.coords(text,tp,tp)
 clr = random_color()
 nclr = random_color()
 
-circle = canvas.create_oval(xp, xp, yp, yp, fill=clr, outline="#000000", width=4)
-circle_shadow = canvas.create_oval(xs,xs,ys,ys,fill="#000000")
-canvas.tag_lower(circle_shadow, None) 
-text = canvas.create_text(tp,tp, text="Random Button", fill=nclr,font=("Arial", 14, "bold"))
-
+start_circle()
 canvas.bind("<ButtonPress-1>", on_click)
 canvas.bind("<ButtonRelease-1>", on_release)
+canvas.bind("<ButtonPress-3>", shape_square)
+canvas.bind("<ButtonPress-2>",shape_circle)
+
 root.mainloop()
